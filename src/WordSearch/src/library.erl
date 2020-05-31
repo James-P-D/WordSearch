69dfcab3a353b7266b3fa982e7f3f39168aadddb
James Dorrington <jdorrington@hotmail.com>
2020-05-31 18:01:03 +0100

    Adding comments to library.erl

--- src/WordSearch/src/library.erl
+++ src/WordSearch/src/library.erl
@@ -1,22 +1,23 @@
 % c("library.erl").
-% library:stuff(["abc", "abcdef"]).
-% library:stuff(["abc", "xyzabcdef"]).
+% library:stuff([N, "abc", "abcdef"]).      % Returns true, N = 0
+% library:stuff([N, "abc", "xyzabcdef"]).   % Returns true, N = 3
+% library:stuff([N, "abc", "xyzabcdef"]).   % Returns false, N = -1
 
 -module(library).
 -export([stuff/2]).
 
-match_start_string([], _) -> true;
-match_start_string(_, []) -> false;
-match_start_string([Head|Tail1], [Head|Tail2]) -> match_start_string(Tail1, Tail2);
-match_start_string(_, _) -> false.
+% Check is parameter1 appears at start of parameter2
+match_start_string([], _) -> true;  % If parameter1 is empty list, then we have already checked all characters, so must be true
+match_start_string(_, []) -> false; % If parameter2 is empty list, then there aren't enough character in parameter2 to match parameter1, so return false
+match_start_string([Head|Tail1], [Head|Tail2]) -> match_start_string(Tail1, Tail2); % If heads of both parameters are equal, then the character matches, so check the tails of both parameters
+match_start_string(_, _) -> false.  % In all other cases, return false
 
-get_tail([_|Tail]) -> Tail.
-
-sub_string(_, _, []) -> -1;
-sub_string(N, Str1, Str2) ->
-    case (match_start_string(Str1, Str2)) of
-        true -> N;
-        false -> sub_string(N + 1, Str1, get_tail(Str2))
+% Check if parameter2 appears in parameter3, setting parameter1 to the offset if true, or -1 otherwise
+sub_string(_, _, []) -> -1;                         % If parameter3 is empty then return -1 since string not found
+sub_string(N, Str1, Str2) ->                        % In all other scenarios
+    case (match_start_string(Str1, Str2)) of        % Check is parameter2 appears at start of parameter3...
+        true -> N;                                  % ..if it does, return true, since we have found the sub-string..
+        false -> sub_string(N + 1, Str1, tl(Str2))  % ..if not, increment N to point to next character in parameter3, and recursively search the tail of parameter3
     end.
 
 display([]) -> [];
