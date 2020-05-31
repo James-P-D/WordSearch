a6a8efbd7efe85acb03c1b05086d91af457fcc04
James Dorrington <jdorrington@hotmail.com>
2020-05-31 18:07:56 +0100

    Replacing tabs with spaces

--- src/WordSearch/src/Main.java
+++ src/WordSearch/src/Main.java
@@ -22,29 +22,29 @@ import com.ericsson.otp.erlang.OtpSelf;
 // https://github.com/nickmcdowall/Erlang-Examples/wiki/Using-Java-to-call-an-Erlang-function
 
 public class Main {
-	private final static int FRAME_WIDTH = 400;
-	private final static int FRAME_HEIGHT = 600;
-	
-	private final static int GRID_WIDTH = 20;
-	private final static int GRID_HEIGHT = 20;	
+    private final static int FRAME_WIDTH = 400;
+    private final static int FRAME_HEIGHT = 600;
+    
+    private final static int GRID_WIDTH = 20;
+    private final static int GRID_HEIGHT = 20;    
 
-	private final static String DEFAULT_WORDS_TEXTFILE = "words.txt";
-	private final static String DEFAULT_COMPUTER_NAME = "DESKTOP-MF9T345";
-	
-	private static JTextArea wordFileTextArea;
-	private static JTextArea computerNameTextArea;
-	private static JButton connectButton;
-	private static JButton solveButton;
-	private static JButton disconnectButton;
+    private final static String DEFAULT_WORDS_TEXTFILE = "words.txt";
+    private final static String DEFAULT_COMPUTER_NAME = "DESKTOP-MF9T345";
+    
+    private static JTextArea wordFileTextArea;
+    private static JTextArea computerNameTextArea;
+    private static JButton connectButton;
+    private static JButton solveButton;
+    private static JButton disconnectButton;
 
-	private static JEditorPane[][] cells = new JEditorPane[GRID_WIDTH][GRID_HEIGHT];
-	
-	private static OtpSelf client;
-	private static OtpPeer server;
-	private static OtpConnection connection;
-	
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Java/Erlang WordSearch Solver");
+    private static JEditorPane[][] cells = new JEditorPane[GRID_WIDTH][GRID_HEIGHT];
+    
+    private static OtpSelf client;
+    private static OtpPeer server;
+    private static OtpConnection connection;
+    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Java/Erlang WordSearch Solver");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setMinimumSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
@@ -70,17 +70,17 @@ public class Main {
         //newContainer.add(middlePanel);
         
         for(int x = 0; x < GRID_WIDTH; x++) {
-        	for(int y = 0; y < GRID_HEIGHT; y++) {
+            for(int y = 0; y < GRID_HEIGHT; y++) {
                 GridBagConstraints contraints = new GridBagConstraints();
                 cells[x][y] = new JEditorPane("text/html", "");
                 cells[x][y].setText("_");
-               	//textArea.setText("<FONT COLOR=\"#000000\">X</FONT>");
-               	//textArea.setText("<FONT COLOR=\"#D7D7D7\">X</FONT>");
+                //textArea.setText("<FONT COLOR=\"#000000\">X</FONT>");
+                //textArea.setText("<FONT COLOR=\"#D7D7D7\">X</FONT>");
                 contraints.weightx = 1;
                 contraints.gridx = x;
                 contraints.gridy = y;
                 newContainer.add(cells[x][y], contraints);
-        	}
+            }
         }
 
         SetCells("____________________" +
@@ -122,54 +122,54 @@ public class Main {
         bottomPanel.add(disconnectButton);
 
         connectButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					client = new OtpSelf("client", "batman");
-					server = new OtpPeer("server@" + computerNameTextArea.getText());
-					connection = client.connect(server);
-					connectButton.setEnabled(false);
-					solveButton.setEnabled(true);
-					disconnectButton.setEnabled(true);
-				} catch (IOException | OtpAuthException e) {
-					JOptionPane.showMessageDialog(null, "Unable to connect to Erlang\n" + e.getMessage());
-					System.out.println(e.getMessage());
-					System.out.println(e.getStackTrace());
-					return;
-				}				
-			}
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    client = new OtpSelf("client", "batman");
+                    server = new OtpPeer("server@" + computerNameTextArea.getText());
+                    connection = client.connect(server);
+                    connectButton.setEnabled(false);
+                    solveButton.setEnabled(true);
+                    disconnectButton.setEnabled(true);
+                } catch (IOException | OtpAuthException e) {
+                    JOptionPane.showMessageDialog(null, "Unable to connect to Erlang\n" + e.getMessage());
+                    System.out.println(e.getMessage());
+                    System.out.println(e.getStackTrace());
+                    return;
+                }                
+            }
         });
 
         solveButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				try {					
-					int[] some_array = new int[3];
-					some_array[0] = 1;
-					some_array[0] = 2;
-					some_array[0] = 3;
-					//connection.sendRPC("translator", "translate", withArgs("friend", "Spanish"));
-					connection.sendRPC("translator", "list_length", GetArguments(some_array));
-					OtpErlangObject response = connection.receiveMsg().getMsg();
-					JOptionPane.showMessageDialog(null, response.toString());
-					solveButton.setEnabled(false);
-				} catch (IOException | OtpErlangDecodeException | OtpErlangExit | OtpAuthException e) {
-					JOptionPane.showMessageDialog(null, "Unable to query Erlang\n" + e.getMessage());
-					System.out.println(e.getMessage());
-					System.out.println(e.getStackTrace());
-					return;
-				}
-			}
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {                    
+                    int[] some_array = new int[3];
+                    some_array[0] = 1;
+                    some_array[0] = 2;
+                    some_array[0] = 3;
+                    //connection.sendRPC("translator", "translate", withArgs("friend", "Spanish"));
+                    connection.sendRPC("translator", "list_length", GetArguments(some_array));
+                    OtpErlangObject response = connection.receiveMsg().getMsg();
+                    JOptionPane.showMessageDialog(null, response.toString());
+                    solveButton.setEnabled(false);
+                } catch (IOException | OtpErlangDecodeException | OtpErlangExit | OtpAuthException e) {
+                    JOptionPane.showMessageDialog(null, "Unable to query Erlang\n" + e.getMessage());
+                    System.out.println(e.getMessage());
+                    System.out.println(e.getStackTrace());
+                    return;
+                }
+            }
         });
 
         disconnectButton.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				connection.close();
-				connectButton.setEnabled(true);
-				solveButton.setEnabled(false);
-				disconnectButton.setEnabled(false);
-			}
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                connection.close();
+                connectButton.setEnabled(true);
+                solveButton.setEnabled(false);
+                disconnectButton.setEnabled(false);
+            }
         });
         
         /////////////////////////////////////////////////////////////////////////////////////
@@ -179,39 +179,39 @@ public class Main {
         frame.getContentPane().add(BorderLayout.SOUTH, bottomPanel);
         
         frame.setVisible(true);
-	}
-	
-	private static OtpErlangObject[] GetArguments(int[] some_array) {
-		OtpErlangObject[] otpErlangObjects = new OtpErlangObject[some_array.length];
-		for (int i = 0; i < some_array.length; i++){
-			otpErlangObjects[i] = new OtpErlangLong(some_array[i]);
-		}
-		
-		return new OtpErlangObject[] { 
-			new OtpErlangList(otpErlangObjects)
-		};
-	}
-	
-	private static void SetCells(String str) {
-		Random rand = new Random(); 
-		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		str = str.toUpperCase();
-		
-        if (str.length() != GRID_WIDTH * GRID_HEIGHT) {
-        	JOptionPane.showMessageDialog(null, "Incorrect board size!");
-        	return;
+    }
+    
+    private static OtpErlangObject[] GetArguments(int[] some_array) {
+        OtpErlangObject[] otpErlangObjects = new OtpErlangObject[some_array.length];
+        for (int i = 0; i < some_array.length; i++){
+            otpErlangObjects[i] = new OtpErlangLong(some_array[i]);
         }
-        	
+        
+        return new OtpErlangObject[] { 
+            new OtpErlangList(otpErlangObjects)
+        };
+    }
+    
+    private static void SetCells(String str) {
+        Random rand = new Random(); 
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        str = str.toUpperCase();
+        
+        if (str.length() != GRID_WIDTH * GRID_HEIGHT) {
+            JOptionPane.showMessageDialog(null, "Incorrect board size!");
+            return;
+        }
+            
         for (int x = 0; x < GRID_WIDTH; x++) {
             for (int y = 0; y < GRID_HEIGHT; y++) {
-            	char ch = str.charAt((y * GRID_HEIGHT) + x);
-            	if (chars.contains(Character.toString(ch))) {
-            		cells[x][y].setText(Character.toString(ch));
-            	} else {            	
-            		char randChar = chars.charAt(rand.nextInt(26));
-            		cells[x][y].setText(Character.toString(randChar));
-            	}
-            }        	
+                char ch = str.charAt((y * GRID_HEIGHT) + x);
+                if (chars.contains(Character.toString(ch))) {
+                    cells[x][y].setText(Character.toString(ch));
+                } else {                
+                    char randChar = chars.charAt(rand.nextInt(26));
+                    cells[x][y].setText(Character.toString(randChar));
+                }
+            }            
         }
-	}
+    }
 }
